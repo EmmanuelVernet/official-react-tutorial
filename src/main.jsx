0f85a3a9b6fb59736bc32279a3b37f1b7d5dedfe
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-03 19:59:14 +0200

    added files for exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 // import App from './App.jsx'
-import App from './exercises/overview/App.jsx'
+// import App from './exercises/overview/App.jsx'
+import App from './exercises/tic-tac-toe/App.jsx'
+import './exercises/tic-tac-toe/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
