c1a8d03d78acf9cdcae29da3822cb9dc963f6b43
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-04 16:07:44 +0200

    added squares state

--- src/exercises/tic-tac-toe/App.jsx
+++ src/exercises/tic-tac-toe/App.jsx
@@ -1,31 +1,35 @@
 import { useState } from "react"
 
-function Square() {
-  const [value, setValue] = useState(null)
+function Square({value}) {
+  // const [value, setValue] = useState(null)
 
   function handleClick(){
-    console.log("clicked")
+    // console.log("clicked")
+    setValue("X")
   }
   return <button className="square" onClick={handleClick}>{value}</button>
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  // console.log(squares)
+
   return(
   <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={[0]} />
+      <Square value={[1]} />
+      <Square value={[2]} />
     </div>
     <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square value={[3]}/>
+      <Square value={[4]}/>
+      <Square value={[5]}/>
     </div>
     <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square value={[6]}/>
+      <Square value={[7]}/>
+      <Square value={[8]}/>
     </div>
   </>
   )
