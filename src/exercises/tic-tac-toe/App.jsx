311dd5eb083d35aa664d18a6c0c05840b6a8216b
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-04 16:26:47 +0200

    added guard clause if click & opponent event

--- src/exercises/tic-tac-toe/App.jsx
+++ src/exercises/tic-tac-toe/App.jsx
@@ -7,14 +7,25 @@ function Square({value, onSquareClick}) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
   // console.log(squares)
   function handleClick(i){
     // console.log("clicked")
     // setValue("X")
+    if (squares[i]) {
+      return
+    }
+    
     const nextSquares = squares.slice()
-    nextSquares[i] = "X"
+    if (xIsNext) {
+      nextSquares[i] = "X"
+    } else {
+      nextSquares[i] = "O"
+    }
+    
     setSquares(nextSquares)
+    setXIsNext(!xIsNext)
   }
   
 
