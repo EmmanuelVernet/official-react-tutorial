a320b0f0957f3801db6c298076c95f71478bef27
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-05 15:25:41 +0200

    added component files for exercise

---
+++ src/exercises/describing-ui/import-export-components/App.jsx
@@ -0,0 +1,15 @@
+// import Gallery from "./Gallery.jsx";
+
+// export default function App() {
+//   return (
+//     <Gallery />
+//   )
+// }
+
+import { Profile } from "./Gallery.jsx"
+
+export default function App() {
+  return (
+    <Profile />
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import { createRoot } from 'react-dom/client'
 // import './exercises/tic-tac-toe/style.css'
 // import App from './exercises/thinking-in-react/App.jsx'
 // import App from './exercises/describing-ui/App.jsx'
-import App from './exercises/describing-ui/first-component/App.jsx'
+// import App from './exercises/describing-ui/first-component/App.jsx'
+import App from './exercises/describing-ui/import-export-components/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
