3020801dbccb4238c5a6c241a2193075493c9b6e
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-05 15:14:21 +0200

    finished examples

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { createRoot } from 'react-dom/client'
 // import App from './exercises/tic-tac-toe/App.jsx'
 // import './exercises/tic-tac-toe/style.css'
 // import App from './exercises/thinking-in-react/App.jsx'
-import App from './exercises/describing-ui/App.jsx'
+// import App from './exercises/describing-ui/App.jsx'
+import App from './exercises/describing-ui/first-component/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
