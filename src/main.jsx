c1730d0793ff38df2034fb48795efdbd9162a759
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-05 14:44:06 +0200

    init exercise folder

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { createRoot } from 'react-dom/client'
 // import App from './exercises/overview/App.jsx'
 // import App from './exercises/tic-tac-toe/App.jsx'
 // import './exercises/tic-tac-toe/style.css'
-import App from './exercises/thinking-in-react/App.jsx'
-// import './exercises/tic-tac-toe/style.css'
+// import App from './exercises/thinking-in-react/App.jsx'
+import App from './exercises/describing-ui/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
