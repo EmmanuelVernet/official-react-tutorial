301f63b8e95b7354ed84a23eaa3a8252f1b6ed2a
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-06 15:32:15 +0200

    refactored code into util files

--- src/exercises/describing-ui/rendering-lists/App.jsx
+++ src/exercises/describing-ui/rendering-lists/App.jsx
@@ -1,14 +1,36 @@
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-];
+// const people = [
+//   'Creola Katherine Johnson: mathematician',
+//   'Mario José Molina-Pasquel Henríquez: chemist',
+//   'Mohammad Abdus Salam: physicist',
+//   'Percy Lavon Julian: chemist',
+//   'Subrahmanyan Chandrasekhar: astrophysicist'
+// ];
+// export default function List() {
+//   const listItems = people.map(person =>
+//     <li>{person}</li>
+//   );
+//   return <ul>{listItems}</ul>;
+// }
+import { people } from './data.jsx';
+import { getImageUrl } from './utils.jsx';
 
 export default function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const listItems = chemists.map(person =>
+    <li>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
   );
   return <ul>{listItems}</ul>;
 }
+
