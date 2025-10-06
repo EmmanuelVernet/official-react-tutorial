d69a7bc45481a73acbc4a562e419d9b38bdc9975
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-06 15:37:42 +0200

    added key prop

--- src/exercises/describing-ui/rendering-lists/App.jsx
+++ src/exercises/describing-ui/rendering-lists/App.jsx
@@ -14,23 +14,39 @@
 import { people } from './data.jsx';
 import { getImageUrl } from './utils.jsx';
 
+// export default function List() {
+//   const chemists = people.filter(person =>
+//     person.profession === 'chemist'
+//   );
+//   const listItems = chemists.map(person =>
+//     <li key={person.id}>
+//       <img
+//         src={getImageUrl(person)}
+//         alt={person.name}
+//       />
+//       <p>
+//         <b>{person.name}:</b>
+//         {' ' + person.profession + ' '}
+//         known for {person.accomplishment}
+//       </p>
+//     </li>
+//   );
+//   return <ul>{listItems}</ul>;
+// }
+
 export default function List() {
-  const chemists = people.filter(person =>
-    person.profession === 'chemist'
-  );
-  const listItems = chemists.map(person =>
-    <li>
+  const listItems = people.map(person =>
+    <li key={person.id}>
       <img
         src={getImageUrl(person)}
         alt={person.name}
       />
       <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
       </p>
     </li>
   );
   return <ul>{listItems}</ul>;
 }
-
