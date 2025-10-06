140aa5821d5a5e229d3a2cf6f3491e6af9bdd1ea
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-06 11:34:43 +0200

    added utils.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import { createRoot } from 'react-dom/client'
 // import App from './exercises/describing-ui/App.jsx'
 // import App from './exercises/describing-ui/first-component/App.jsx'
 // import App from './exercises/describing-ui/import-export-components/App.jsx'
-import App from './exercises/describing-ui/js-in-jsx/App.jsx'
+// import App from './exercises/describing-ui/js-in-jsx/App.jsx'
+import App from './exercises/describing-ui/passing-props/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
