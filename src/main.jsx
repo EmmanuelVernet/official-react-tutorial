1b1b35c17352fc77de9c2c2bc5d9a1bcc18278ca
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-06 12:05:26 +0200

    added contional-rendering exercise file

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import { createRoot } from 'react-dom/client'
 // import App from './exercises/describing-ui/first-component/App.jsx'
 // import App from './exercises/describing-ui/import-export-components/App.jsx'
 // import App from './exercises/describing-ui/js-in-jsx/App.jsx'
-import App from './exercises/describing-ui/passing-props/App.jsx'
+// import App from './exercises/describing-ui/passing-props/App.jsx'
+import App from './exercises/describing-ui/conditional-rendering/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
