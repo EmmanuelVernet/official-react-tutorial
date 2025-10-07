fcd297b1f9fa3fee0d6d885e9404ea345d546cc3
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-07 18:00:11 +0200

    testing example for alert buttons

--- src/exercises/adding-interactivity/responding-to-events/App.jsx
+++ src/exercises/adding-interactivity/responding-to-events/App.jsx
@@ -1,4 +1,4 @@
-export default function Button() {
+// export default function Button() {
   // function handleClick() {
   //   alert("clicked")
   // }
@@ -9,9 +9,32 @@ export default function Button() {
   //   </button>
   // );
 
-  return (
-    <button onClick={() => { alert('clicked')}}>
-      click me
-    </button>
-  );
-}
+//   return (
+//     <button onClick={() => { alert('clicked')}}>
+//       click me
+//     </button>
+//   );
+// }
+
+// function AlertButton({ message, children }) {
+//   return (
+//     <button onClick={() => alert(message)}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <AlertButton message="Playing!">
+//         Play
+//       </AlertButton>
+//       <AlertButton message="Uploading!">
+//         Upload
+//       </AlertButton>
+//     </div>
+//   );
+// }
+
+
