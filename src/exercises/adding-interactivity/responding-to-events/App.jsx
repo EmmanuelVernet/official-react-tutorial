80940dd8f6e0f2a20a4d3d91387125417c878168
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-07 18:09:51 +0200

    next buttons example

--- src/exercises/adding-interactivity/responding-to-events/App.jsx
+++ src/exercises/adding-interactivity/responding-to-events/App.jsx
@@ -37,4 +37,39 @@
 //   );
 // }
 
+function Button({ onClick, children }) {
+  console.log(children)
+  return(
+    <button onClick={onClick}>{children}</button>
+  )
+}
 
+function PlayButton( {movieName} ) {
+  
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`)
+  }
+
+  return(
+    <Button onClick={handlePlayClick}>
+      Play '{movieName}'
+    </Button>
+  )
+}
+
+function UploadButton() {
+  return(
+    <Button onClick={() => alert('Uploading')}>
+      Upload image
+    </Button>
+  )
+}
+
+export default function Toolbar() {
+  return(
+    <div>
+      <PlayButton movieName="Kiki's Delivery" />
+      <UploadButton/>
+    </div>
+  )
+}
